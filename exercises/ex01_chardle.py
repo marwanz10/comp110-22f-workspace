5dd272c7b2cc8e745cb0a2da88492e631fc0dc5a
Marwan Zarrouk <marwanz@unc.edu>
2022-10-24 13:38:34 -0400

    new updates

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -8,26 +8,30 @@ if len(word) != 5:
 else:
     letter: str = input("Enter a single character: ")
     if len(letter) != 1:
-        print("Character must be a single character. " )
+        print("Character must be a single character.")
     else:
-        print("Searching for " + letter +  " in " + word)
+        print("Searching for " + letter +  "in " + word)
         if letter == word [0] :
-            print ( letter + " found at index 0 " )
+            print ( letter + "found at index 0 " )
         if letter == word [1] :
-            print ( letter + " found at index 1 " )
+            print ( letter + "found at index 1 " )
         if letter == word [2] :
-            print ( letter + " found at index 2 " )
+            print ( letter + "found at index 2 " )
         if letter == word [3] :
-            print ( letter + " found at index 3 " )
+            print ( letter + "found at index 3 " )
         if letter == word [4] :
-            print ( letter + " found at index 4 " )
+            print ( letter + "found at index 4 " )
         
         my_counter = word.count(letter)
         if my_counter == 0:
             print("No instances of " + letter + "found in " + word)
         else:
-            print (str(my_counter ) + " instances of " + letter + " found in " + word )
-    
+            if my_counter == 1:
+             print ("1 instance of " + letter + " found in " + word)
+        
+            else:
+                print (str(my_counter ) + "instances of " + letter + "found in " + word )
+            
 
 
         
